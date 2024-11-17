b0105e04924d5cfad4b740e32d9a7acfea6de66f
Govind ghosh <govindghosh0@gmail.com>
2024-11-17 10:56:06 +0530

    [lear]PrimitivePrint

--- first-day/src/Inputs.java
+++ first-day/src/Inputs.java
@@ -4,16 +4,41 @@ public class Inputs {
     public static void main(String[] args) {
         System.out.println("Welcome to our roll number printer");
         Scanner input = new Scanner(System.in);
+
+        // Reading roll number
         System.out.print("Please Enter your roll number: ");
         int rollno = input.nextInt();
+        input.nextLine(); // Clear the newline character left by nextInt()
         System.out.println("Your Roll number is " + rollno);
-        //String
-        Scanner inputFirstName = new Scanner(System.in);
-        System.out.print("Please enter your name: ");
-        String firstName = inputFirstName.nextLine();
-        Scanner inputSurname = new Scanner(System.in);
-        System.out.print("Please enter your surname: ");
-        String surname = inputSurname.nextLine();
-        System.out.println("your name is " + firstName + surname);
+
+        // Reading first name
+        System.out.println("Please enter your name: ");
+        String firstName = input.nextLine();
+        System.out.println("Thank you for mentioning your name.");
+
+        // Reading surname
+        System.out.println("Please enter your surname: ");
+        String surname = input.nextLine();
+        System.out.println("Thank you for mentioning your surname.");
+
+        // Printing full name
+        System.out.println("Your full name is " + firstName + " " + surname);
+
+        // Primitives print
+        System.out.println("please write a integers");
+        int intNumber = input.nextInt();
+        System.out.println("Please write a Long");
+        long longNumber = input.nextLong();
+        System.out.println("Please write a Float");
+        float floatNumber = input.nextFloat();
+        System.out.println("please write a double");
+        double doubleNumber = input.nextDouble();
+        System.out.println("please write a char");
+        char charIs = input.next().charAt(0);
+        System.out.println("your int is " + intNumber +
+                ", long Number is " + longNumber +
+                ", float number is " + floatNumber +
+                ", double number is " + doubleNumber +
+                ", chat is " + charIs);
     }
 }
