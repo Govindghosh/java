4d8872d66d102e34bcfb4d4f36d0f84dda0f6f86
Govind ghosh <govindghosh0@gmail.com>
2024-12-13 19:13:31 +0530

    [Learn]isPrime

---
+++ conditional/src/function/Question.java
@@ -0,0 +1,27 @@
+package function;
+
+import java.util.Scanner;
+
+public class Question {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter number to find IsPrime :");
+        int n = in.nextInt();
+        boolean ans = isPrime(n);
+        System.out.println(ans);
+        in.close();
+    }
+    static boolean isPrime(int n){
+        if (n <= 1){
+            return false;
+        }
+        int c = 2;
+        while (c * c <= n){
+            if (n % c == 0){
+                return false;
+            }
+            c++;
+        }
+        return c * c > n;
+    }
+}
