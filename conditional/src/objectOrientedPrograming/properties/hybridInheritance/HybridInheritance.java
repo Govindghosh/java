53be8822047513bf0e4463be8f51a41686edf3bc
Govind ghosh <govindghosh0@gmail.com>
2025-02-16 15:18:01 +0530

    [Lear]hybridInheritance

---
+++ conditional/src/objectOrientedPrograming/properties/hybridInheritance/HybridInheritance.java
@@ -0,0 +1,9 @@
+package objectOrientedPrograming.properties.hybridInheritance;
+//Hybrid Inheritance in Java 🚀
+//Hybrid inheritance is a combination of multiple types of inheritance, such as single, hierarchical,
+//and multiple inheritance. However, Java does not support hybrid inheritance with classes directly because multiple
+//inheritance using classes is not allowed (to avoid ambiguity and the diamond problem).
+//But hybrid inheritance can be achieved using interfaces since Java allows multiple inheritance with interfaces.
+public class HybridInheritance {
+    // not allow because multiple inheritance not allow
+}
